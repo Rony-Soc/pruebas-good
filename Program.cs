b755fdef8a83d9cfe960856e1aac091a735593fb
Rony-Soc <147260251+Rony-Soc@users.noreply.github.com>
2025-08-04 17:15:49 -0600

    Update Program.cs
    funcion

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("holas Zonia");
+Console.WriteLine("ingrese edad");
+int edad = Console.ReadLine();
 
+console.writeLine("Su edad es de",edad);
 
